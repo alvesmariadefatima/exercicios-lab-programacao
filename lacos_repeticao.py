4a7363291dda98b7f3d30bc298b4cb094ca66695
Maria de Fátima Nunes Alves <94319702+mariadefatima1234@users.noreply.github.com>
2022-02-23 07:53:38 -0300

    Exercício Laços de Repetição for em Python

---
+++ lacos_repeticao.py
@@ -0,0 +1,25 @@
+#Desafio 02 - Laços de repetição
+
+a, b = input().split()
+a, b = int(a), int(b)
+media = (a+b)/2
+print(media)
+
+a, b = input().split()
+a, b = int(a),int(b)
+media = (a+b)/2
+print(media)
+
+a, b = input().split()
+a, b = int(a), int(b)
+media = (a+b)/2
+print(media)
+
+
+for i in range(3):
+  a, b = input().split()
+  a, b = int(a), int(b)
+  media = (a+b)/2
+  print(media)
+  print("LOOP", i)
+   
